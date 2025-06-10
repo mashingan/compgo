6e3dd693123408e3c3fce321edbf4aae646de8a2
mashingan <rahmat.d.ruffy@gmail.com>
2025-06-11 01:22:28 +0700

    add vm test fibonacci recursive

--- Go/compgo/comp/vm_test.go
+++ Go/compgo/comp/vm_test.go
@@ -497,3 +497,22 @@ func TestClosureVm_recursive(t *testing.T) {
 	}
 	runVmTests(t, tests)
 }
+
+func TestRecusiveFibonacci(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+let fibonacci = fn(x) {
+	if (x == 0) { return 0 }
+	else {
+		if (x == 1) { return 1; }
+		else { fibonacci(x - 1) + fibonacci(x - 2) }
+	}
+};
+fibonacci(15);
+			`,
+			expected: 610,
+		},
+	}
+	runVmTests(t, tests)
+}
